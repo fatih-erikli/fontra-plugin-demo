c5e122a666e48a3b26d0b6f579920bc4ab5f0efd
Fatih Erikli <92447202+fatih-erikli@users.noreply.github.com>
2023-08-14 01:36:26 +0300

    Update start.js

--- start.js
+++ start.js
@@ -1,24 +1,20 @@
 class FontraPlugin {
   main(fontra) {
-    fontra.addSidebarPanel(
-      new (class {
-        name = "my-panel";
-        icon = "/images/gear.svg";
-
-        getContentElement() {
-          const div = document.createElement("div");
-          div.innerHTML = "My plugin";
-          return div;
-        }
-
-        attach() {
-          console.log("Hello fontra");
-        }
-
-        detach() {
-          console.log("Goodbye fontra");
-        }
-      })(),
+    fontra.addSidebarPanel({
+      name: "my-panel",
+      icon: "/images/gear.svg",
+      getContentElement() {
+        const div = document.createElement("div");
+        div.innerHTML = "My plugin";
+        return div;
+      },
+      attach() {
+        console.log("Hello fontra");
+      },
+      detach() {
+        console.log("Goodbye fontra");
+      }
+    },
       "right"
     );
   }
