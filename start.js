656bb5b8c5f47b96f35cb6b0aa1bce7ae0a4e775
Fatih Erikli <92447202+fatih-erikli@users.noreply.github.com>
2023-08-13 19:36:31 +0300

    Update start.js

--- start.js
+++ start.js
@@ -1,4 +1,7 @@
 class Panel {
+  name = "my-panel";
+  icon = "https://www.svgrepo.com/show/506176/cut.svg";
+
   getContentElement() {
     const div = document.createElement("div");
     div.innerHTML = "My plugin"
