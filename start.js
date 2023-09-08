6d127b978a19bcaf7a4c9c2dd9fd3aa7e0fc7ba6
Fatih Erikli <92447202+fatih-erikli@users.noreply.github.com>
2023-09-08 23:48:56 +0300

    Update start.js

--- start.js
+++ start.js
@@ -1,24 +1,3 @@
-class FontraPlugin {
-  enter() {
-    console.log("Hello fontra");
-  }
-  exit() {
-    console.log("Goodbye fontra");
-  }
-  main(fontra) {
-    fontra.addSidebarPanel({
-      name: "my-panel",
-      icon: "/images/gear.svg",
-      getContentElement() {
-        const div = document.createElement("div");
-        div.innerHTML = "My plugin";
-        return div;
-      },
-      attach() {
-        
-      }
-    },
-      "right"
-    );
-  }
+export function start(editorController) {
+  console.log('access to editorController instance from plugin', editorController);
 }
